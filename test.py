9be76e507392acfef434f4f56ec5c1781e52106c
Abhinav Yukth S <93666264+Abhinavyukth-Suresh@users.noreply.github.com>
2023-12-02 23:15:53 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     brenner.restype = ctypes.POINTER(ctypes.c_float * k)
 
     c1,c2,w,chi, = 0.15, 0.052, 0.3, 1.0,
-    SwarmSize,upper,lower,n_iter = 200,15,0,1000
+    SwarmSize,upper,lower,n_iter = 200,15,0,20
     print(c1,c2,w,chi,SwarmSize,upper,lower,n_iter)
     a = [i for i in brenner(ctypes.c_float(c1),ctypes.c_float(c2),ctypes.c_float(w),ctypes.c_float(chi),
                             ctypes.c_size_t(SwarmSize),ctypes.c_float(upper),ctypes.c_float(lower),ctypes.c_int(n_iter)).contents]
